302ee5b0c9d5ccd2cb1deee5ca5f3e11eaa3ebd3
Aman Agrawal <amanagr@zulip.com>
2020-06-02 13:33:20 +0530

    to_dict_uncached: Pipe realm_id from the caller functions.
    We pipe realm_id through functions where it is available,
    this helps us avoid doing query for realm_id in loop when
    multiple messages are being processed.

--- zerver/tests/test_subs.py
+++ zerver/tests/test_subs.py
@@ -2540,7 +2540,7 @@ class SubscriptionAPITest(ZulipTestCase):
                     streams_to_sub,
                     dict(principals=ujson.dumps([user1.id, user2.id])),
                 )
-        self.assert_length(queries, 40)
+        self.assert_length(queries, 39)
 
         self.assert_length(events, 7)
         for ev in [x for x in events if x['event']['type'] not in ('message', 'stream')]:
@@ -3321,7 +3321,7 @@ class SubscriptionAPITest(ZulipTestCase):
                 [new_streams[0]],
                 dict(principals=ujson.dumps([user1.id, user2.id])),
             )
-        self.assert_length(queries, 40)
+        self.assert_length(queries, 39)
 
         # Test creating private stream.
         with queries_captured() as queries:
@@ -3331,7 +3331,7 @@ class SubscriptionAPITest(ZulipTestCase):
                 dict(principals=ujson.dumps([user1.id, user2.id])),
                 invite_only=True,
             )
-        self.assert_length(queries, 40)
+        self.assert_length(queries, 39)
 
         # Test creating a public stream with announce when realm has a notification stream.
         notifications_stream = get_stream(self.streams[0], self.test_realm)
@@ -3346,7 +3346,7 @@ class SubscriptionAPITest(ZulipTestCase):
                     principals=ujson.dumps([user1.id, user2.id])
                 )
             )
-        self.assert_length(queries, 52)
+        self.assert_length(queries, 50)
 
 class GetStreamsTest(ZulipTestCase):
     def test_streams_api_for_bot_owners(self) -> None:
